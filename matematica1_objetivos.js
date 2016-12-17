ed5fd55b27636b003c41948a1036bae0c87c430c
unamatematicaseltigre <jlaurentum@gmail.com>
2016-12-17 00:40:11 -0400

    Add files via upload

---
+++ matematica1_objetivos.js
@@ -0,0 +1,101 @@
+// Encoding : UTF-8
+
+objetivos = {
+	"1" : "Efectuar problemas relacionados con los números naturales, enteros o 	racionales que involucren operaciones definidas o cálculos directos utilizando calculadora en esos conjuntos.",
+	"2" : "Efectuar problemas relacionados con los números reales que involucren operaciones definidas o cálculos directos utilizando calculadora en ese conjunto.",
+	"3" : "Efectuar problemas que involucren la relación de orden en \\(\\mathbb{R}\\).",
+	"4" : "Efectuar problemas que involucren representaciones de puntos de una recta o de un plano utilizando sistemas de coordenadas, relaciones entre conjuntos o funciones.",
+	"5" : "Efectuar problemas distinguiendo diversas características de una función, geométrica y analíticamente o relacionados con la composición de funciones.",
+	"6" : "Efectuar problemas que impliquen la representación de datos utilizando diferentes gráficas o relacionados con la noción de escala.",
+	"7" : "Efectuar problemas de aproximación sucesiones y límites de sucesiones.",
+	"8" : "Efectuar problemas que involucren límites de funciones.",
+	"9" : "Efectuar problemas concernientes a las funciones continuas."
+};
+
+//A continuación, oe_i son los objetivos específicos del objetivo general i
+oe1 = [
+	// 0
+	"Resolver problemas que involucren números <b>naturales</b> o <b>enteros</b> mediante ecuaciones y la aplicación de operaciones y propiedades sobre esos conjuntos (Módulo I, Unidad 1, 1.1-1.3).",
+	// 1
+	"Definir el conjunto de <b>números racionales</b> y determinar si un número es racional o no (Módulo I, Unidad 1, 1.4.1 y 1.4.2 y Módulo I, Unidad 2, 2.1 y 2.2).",
+	// 2
+	"Obtener la <b>parte entera</b> y la <b>parte decimal</b> de un número racional (Módulo I, Unidad 1, 1.4.2, pp. 58-59).",
+	// 3
+	"Expresar números <b>decimales</b> en <b>notación científica</b> y vice-versa (Módulo I, Unidad 1, 1.4.2).",
+	// 4
+	"Determinar si un <b>número racional</b> es <b>número decimal</b> (Módulo I, Unidad 1, 1.4.2).",
+	// 5
+	"Determinar el <b>periodo</b> de una <b>expresión decimal periódica</b> (Módulo I, Unidad 1, 1.5).",
+	// 6
+	"Usar la calculadora en cálculos con números racionales y manejar los conceptos/procedimientos de <b>truncamiento</b>, <b>redondeo</b>, <b>redondeo por defecto o por exceso</b> y de <b>cifras significativas</b> en la <b>aproximación</b> de estos cálculos (Módulo I, Unidad 1, 1.5).",
+	// 7
+	"Manejar las propiedades de las operaciones con números racionales (Módulo I, Unidad 1, 1.6.1).",
+	// 8
+	"Comprender la relación de orden en los números racionales y calcular la <b>media aritmética</b>, <b>promedio</b> o <b>semisuma</b> entre dos números racionales (Módulo I, Unidad 1, 1.6.2).",
+	// 9
+	"Resolver problemas que involucren operaciones y ecuaciones con números racionales (Módulo 1, Unidad 1, 1.7 y 1.8)."
+];
+
+oe2 = [
+	// 0
+	"Determinar si un número real es irracional o racional (Módulo I, Unidad 2, 2.2).", // 1
+	"Caracterizar al conjunto de los números reales y sus subconjuntos (Módulo I, Unidad 2, 2.2.2).",
+	// 2
+	"Manejar las propiedades de las operaciones con los números reales (Módulo I, Unidad 2, 2.3).",
+	// 3
+	"Resolver problemas de cálculo que involucren operaciones de aproximaciones decimales de números reales (Módulo I, Unidad 2, 2.4)."
+];
+
+oe3 = [
+	// 0
+	"Manejar el concepto de relación de orden en \\(\\mathbb{R}\\) y sus propiedades (Módulo I, Unidad 3, 3.2).",
+	// 1
+	"Manejar el concepto de valor absoluto de un número real, distancia en \\(\\mathbb{R}\\) y sus propiedades en la resolución de problemas (Módulo I, Unidad 3, 3.3).",
+	// 2
+	"Expresar la solución de inecuaciones en notación de intervalo (Módulo I, Unidad 3, 3.4).",
+	// 3
+	"Resolver inecuaciones de primer grado (Módulo I, Unidad 3, 3.5.2, Ejemplos 3.5.3 y 3.5.4).",
+	// 4
+	"Resolver inecuaciones con valor absoluto (Módulo I, Unidad 3, 3.5.2, Ejemplos 3.5.5).",
+	// 5
+	"Resolver inecuaciones cuadráticas (de segundo grado) (Módulo I, Unidad 3, 3.5.2, Ejemplos 3.5.6)."
+];
+
+oe4 = [
+	// 0
+	"Manejar el concepto de sistema de coordenadas en dos dimensiones y plano cartesiano. Ubicar puntos en un sistema de coordenadas e identificar los cuadrantes (Módulo II, Unidad 4, 4.2.2).",
+	// 1
+	"Determinar la distancia o el punto medio entre dos puntos en el plano (Módulo II, Unidad 4, 4.3).",
+	// 2
+	"Representar subconjuntos del plano dados por ecuaciones con dos variables (Módulo II, Unidad 4, 4.4).",
+	// 3
+	"Resolver problemas que involucren rectas y semiplanos en el plano cartesiano así como manejar los conceptos de: pendiente, inclinación, ecuaciones de la recta (ecuación general, ecuación pendiente-punto, ecuación punto-punto), rectas paralelas y perpendiculares (Módulo II, Unidad 4, 4.5).",
+	// 4
+	"Manejar las nociones y conceptos relativos a las funciones y relaciones: par ordenado, producto cartesiano, variables dependientes e independientes, dominio, codominio, imágen y rango de una función (Módulo II, Unidad 4, 4.6)."
+];
+
+oe5 = [
+	// 0
+	"Manejar las distintas formas de representar una función: enunciado verbal, fórmulas, gráfica y mediante tabla de valores (Módulo II, Unidad 5, 5.2).",
+	// 1
+	"Conocer las funciones elementales (lineales, polinómicas, exponenciales, logarítmicas, trigonométricas, valor absoluto), sus características (dominio y rango) y ejemplos de aplicación (Módulo II, Unidad 5, 5.3).",
+	// 2
+	"Resolver problemas que involucren obtención de gráficas de funciones mediante traslación de otras y funciones definidas por trozos o secciones (Módulo II, Unidad 5, 5.3).",
+	// 3
+	"Determinar las características de una función dada tales como: propiedades de simetría (funciones pares e impares), propiedades de crecimiento y decrecimiento, periodicidad (Módulo II, Unidad 5, 5.4).",
+	// 4
+	"Utilizar el álgebra de funciones y la composición de funciones para obtener una función a partir de otras (Módulo II, Unidad 5, 5.5).",
+	// 5
+	"Determinar si una función es inyectiva, sobreyectiva o biyectiva así como su función inversa (Módulo II, Unidad 5, 5.5)."
+];
+
+oe6 = [
+	"Definir los siguientes conceptos: dato, variable continua, variable discreta, intervalos de clase, marca de clase, frecuencia absoluta, frecuencia acumulada, proporción y porcentaje de una clase, amplitud y marca de clase, histograma, poligono de frecuencia, diagrama de frecuencias acumuladas, diagrama de barras agrupadas, gráfico de torta," 
+];
+
+oe7 = [
+	//0
+	"Determinar los términos de una sucesión a partir de la fórmula del término general (Módulo III, Unidad 7, 7.2).",
+	//1
+	"Calcular límites de sucesiones utilizando el álgebra de límites (Módulo III, Unidad 7, 7.8).",
+];
